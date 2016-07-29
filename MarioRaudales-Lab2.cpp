a72d3fcde7a94592971920f06740d64fd48d9ac2
MarioRaudales23 <marioraudales33@unitec.edu>
2016-07-29 16:30:21 -0600

    funciona el segundo ejercicio

--- MarioRaudales-Lab2.cpp
+++ MarioRaudales-Lab2.cpp
@@ -3,19 +3,20 @@
 
 int factorial(int);
 int dividir(int,int);
-
+int fac(int);
 int main(){
 	int numerorecibido;
 	int opcionmenu;
 	int sumafac;
 	int numeroamigo;
+	int temporal;
 	char resp;
 	do{
 		std::cout<<"Opcion 1: FActorial\nOpcion 2: Divisor\nIngrese opcion: ";
 		std::cin>>opcionmenu;
 		switch(opcionmenu){
 			case 1:
-				for(int i = 1;i < 50000;i++){
+				for(int i = 1;i <= 50000;i++){
 					sumafac = factorial(i);
 					if(sumafac == i){
 						std::cout<<i<<"\n";
@@ -23,10 +24,16 @@ int main(){
 				}
 				break;
 			case 2:
-				std::cout<<"Ingrese el numero a evaluar";
+				std::cout<<"Ingrese el numero a evaluar: ";
 				std::cin>>numerorecibido;
-				std::cout<<"Ingrese el segundo numero";
+				std::cout<<"Ingrese el segundo numero: ";
 				std::cin>>numeroamigo;
+				temporal = dividir(numerorecibido,numeroamigo);
+				if(temporal == 1){
+					std::cout<<"Los numeros son amigos"; 
+				}else{
+					std::cout<<"No son amigos";	
+				}
 				break;
 			default:
 				std::cout<<"Opcion no valida"; 
@@ -39,23 +46,40 @@ int main(){
 
 
 int factorial(int m){
-	int residuo = m%10;
-	int sumatoriaresiduo = 0;
-	int restante = m/10;
-	do{
-		
-		int factorialtemporal = 1;
-		for(int i=1;i<=residuo;i++){
-			factorialtemporal*=i;
-		}
-		sumatoriaresiduo += factorialtemporal;
-		residuo = restante%10;
-		restante /=10;
-	}while(residuo != 0);
-	return sumatoriaresiduo;
+	int residuo = 0;
+	int acumulador = 0;
+	while(m > 0){		
+		residuo = m%10;
+		acumulador += fac(residuo);
+		m = m/10;
+	}
+	return acumulador;
 }
-/*int dividir(int primero,int segundo){
+int dividir(int primero,int segundo){
 	int divisorp;
 	int divisors;
-	for(int i = 0;);
-}*/
+	for(int i = 1;i <primero;i++ ){
+		if(primero%i == 0){
+			divisorp+=i;
+		}
+	}
+	for(int i= 1;i<segundo;i++){
+		if(segundo%i==0){
+			divisors+=i;
+		}
+	}
+
+	if((primero == divisors)&&(segundo==divisorp)){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+int fac(int a){
+	int acumular = 1;
+	for(int i = 1;i <= a;i++){
+		acumular=acumular*i;
+	}
+	return acumular;
+}
+
